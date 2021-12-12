130d743611c485a2f77bea8ee4f2c6426ca432b6
ssolson <ssolson@sandia.gov>
2021-12-11 22:13:55 -0700

    Add folders

---
+++ packages/hardhat/contracts/CrownToken.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.4;
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// https://docs.openzeppelin.com/contracts/3.x/erc20
+
+contract CrownToken is Ownable, ERC20 {
+
+     constructor() public ERC20("Crown Capital DAO", "CROWN") {
+        _mint(msg.sender, 1000 * 10 ** 18);
+    }
+}
