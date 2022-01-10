3999c6280f46fe77e086e47341401c46ca745ca3
ssolson <ssolson@users.noreply.github.com>
2022-01-09 21:27:31 -0700

    Add Mock LP Farm Tests (#3)
    * Enable vault to send to multiple contracts.
    
    * Uncomment 4 staker test
    
    * Add basic reversion tests
    
    * More basic tests around new vault functionality
    
    * create getCurrenttime function in within test.
    
    * Start test by checking public vs onlyOwner access
    
    * Finalize emissions tests from vault
    
    * kill farm test
    
    * Improved and expanded single farm tests
    
    * cleaned updated single farm tests
    
    * isFarmActive function, add % to farm emissions
    
    * copy zach's changes
    
    * Add contract requirements
    
    * minor typo fix
    
    * Adjust tokens per second calculations. Clean up functions.
    
    * Add mock token and farm. Add multifarm tests.
    
    * merge main
    
    * Modify to remove runtime compilation warnings

--- packages/hardhat/contracts/mockSushiLPToken.sol
+++ packages/hardhat/contracts/mockSushiLPToken.sol
@@ -5,9 +5,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // https://docs.openzeppelin.com/contracts/3.x/erc20
 
-contract CrownToken is Ownable, ERC20 {
-
-     constructor() ERC20("Crown Capital Token", "CROWN") {
+contract MockSushiLP is Ownable, ERC20 {
+     constructor() ERC20("Mock Sushi LP", "SushiLP") {
         _mint(msg.sender, 100000000 * 10**18);
     }
 }
