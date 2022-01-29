73fff01d1c1960d93934217e95649d1ffcd2f2e8
ssolson <ssolson@sandia.gov>
2022-01-29 13:12:12 -0700

    TOK-01 | Too Many Digits

--- packages/hardhat/contracts/CrownToken.sol
+++ packages/hardhat/contracts/CrownToken.sol
@@ -8,6 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract CrownToken is Ownable, ERC20 {
 
     constructor() ERC20("Crown Capital Token", "CROWN") {
-        _mint(msg.sender, 100000000 * 10 ** 18);
+        _mint(msg.sender, 1e8 * 10**18);
     }
 }
