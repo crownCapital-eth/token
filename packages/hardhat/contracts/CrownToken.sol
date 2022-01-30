cf6d69ec7be6738e609aeafa0801adedb5e9004b
ssolson <ssolson@sandia.gov>
2022-01-30 12:41:01 -0700

    Add ERC20Votes

--- packages/hardhat/contracts/CrownToken.sol
+++ packages/hardhat/contracts/CrownToken.sol
@@ -1,13 +1,33 @@
 pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// https://docs.openzeppelin.com/contracts/3.x/erc20
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract CrownToken is Ownable, ERC20 {
+contract CrownToken is ERC20, ERC20Permit, ERC20Votes {
 
-    constructor() ERC20("Crown Capital Token", "CROWN") {
+    constructor() ERC20("Crown Capital Token", "CROWN") ERC20Permit("Crown Capital Token"){
         _mint(msg.sender, 1e8 * 10**18);
+    }
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
     }
 }
