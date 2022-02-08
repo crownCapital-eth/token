8c0f059d11ce27d95d5aea4e758da9e694be83bf
ZachSand <zachsand@utexas.edu>
2022-02-08 16:56:16 -0600

    Move contract functions hardcoded strings to the constants

--- packages/react-app/src/components/CrownBalance.jsx
+++ packages/react-app/src/components/CrownBalance.jsx
@@ -1,10 +1,10 @@
 import { Balance } from "./index";
 import { Card } from "antd";
 import { useContractReader } from "eth-hooks";
-import { CROWN_TOKEN_CONTRACT } from "../constants";
+import { BALANCE_OF_FUNCTION, CROWN_TOKEN_CONTRACT } from "../constants";
 
 export default function CrownBalance({ address, readContracts }) {
-  const CrownTokenBalance = useContractReader(readContracts, CROWN_TOKEN_CONTRACT, "balanceOf", [address]);
+  const CrownTokenBalance = useContractReader(readContracts, CROWN_TOKEN_CONTRACT, BALANCE_OF_FUNCTION, [address]);
 
   return (
     <Card title="Crown Tokens Owned" className={"ant-card-small"}>
