0fb64d813ba96763cfae1837001a6998ed3fc579
ZachSand <zachsand@utexas.edu>
2022-02-10 14:10:22 -0600

    Attempt to fix aws deploy issue with deploy script

--- packages/react-app/scripts/deploy.sh
+++ packages/react-app/scripts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /root/.bash_profile
-yarn --cwd ../ install && yarn --cwd ../ build && cp -a ../build/. /var/www/html
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+yarn --cwd "$SCRIPT_DIR/.." install && yarn --cwd "$SCRIPT_DIR/.." build && cp -a "$SCRIPT_DIR/../build/." /var/www/html
