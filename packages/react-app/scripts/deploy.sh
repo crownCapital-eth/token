1c820215896df551d234b0a276e8048ffe2ea40f
ZachSand <zachsand@utexas.edu>
2022-02-10 12:53:23 -0600

    Attempt to fix yarn not found error with aws deploy

--- packages/react-app/scripts/deploy.sh
+++ packages/react-app/scripts/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yarn install && yarn build && cp -a ../build/. /var/www/html
+/root/.nvm/versions/node/v16.14.0/bin/yarn install && /root/.nvm/versions/node/v16.14.0/bin/yarn build && cp -a ../build/. /var/www/html
